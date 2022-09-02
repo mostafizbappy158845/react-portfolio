1079292b08ee67a333cc7d6440fc1c71c0291d03
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2022-09-02 23:34:34 +0600

    navbar-section done

---
+++ src/components/HeroImg/HeroImg.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./HeroImg.css"
+
+const HeroImg = () => {
+  return (
+    <div>HeroImg</div>
+  )
+}
+
+export default HeroImg
---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroImg from '../components/HeroImg/HeroImg'
+import Navbar from '../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <HeroImg/>
+    </div>
+  )
+}
+
+export default Home
