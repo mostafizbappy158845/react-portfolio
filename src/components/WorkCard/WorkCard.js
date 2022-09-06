fd5608ef857274db6541275b0cdb0bfaf9d5a50a
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2022-09-06 19:55:02 +0600

    workCard>p-title issue solve

--- src/components/WorkCard/WorkCard.js
+++ src/components/WorkCard/WorkCard.js
@@ -7,8 +7,8 @@ const WorkCard = (props) => {
     <div className="project-card">
                 <img src={props.imgsrc} alt="imageOfProject" />
                 <h2 className="project-title">{props.title}</h2>
-                <div className="pro-details" title={props.text}>
-                    <p>{props.text.length > 90 ? props.text.slice(0,90) + ' ...' : props.text}</p>
+                <div className="pro-details" >
+                    <p title={props.text}>{props.text.length > 90 ? props.text.slice(0,90) + ' ...' : props.text}</p>
                     <div className="pro-btns">
                         {/* <NavLink to={props.view} className="btn">View</NavLink> */}
                         <a href={props.view}  className="btn" >View</a>
